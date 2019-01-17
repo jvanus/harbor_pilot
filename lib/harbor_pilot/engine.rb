6c91c2975e2ad7533cb6a1b9d0864307b861c961
Jacob Vanus <jvanus@gmail.com>
2019-01-16 19:26:13 -0700

    Update engine with rootpath from config

--- lib/harbor_pilot/engine.rb
+++ lib/harbor_pilot/engine.rb
@@ -1,5 +1,16 @@
 module HarborPilot
   class Engine < ::Rails::Engine
     isolate_namespace HarborPilot
+
+    def engine_file(path)
+      HarborPilot::Engine.root.join path
+    end
+
+    initializer 'harbor_pilot.configuration' do |app|
+      config = HarborPilot.configuration
+        app.routes.append do
+          mount HarborPilot::Engine => config.route_path
+        end
+    end
   end
 end
